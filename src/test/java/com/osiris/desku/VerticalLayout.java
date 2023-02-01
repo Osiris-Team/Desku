bff30a971d9e8b8cdc9c6c4f5d8e722e4d0cf57d
Osiris Team <osiris_support@pm.me>
2023-02-01 14:15:41 +0100

    docs

--- src/test/java/com/osiris/desku/VerticalLayout.java
+++ src/test/java/com/osiris/desku/VerticalLayout.java
@@ -15,7 +15,7 @@ public class VerticalLayout extends Component<VerticalLayout> {
                     "display: flex;" +
                     "flex-direction: column;" +
                     "}";
-            // You can also add a css file the current classes' package/folder.
+            // You can also add a css file to the current classes' package/folder.
             // The file below is at "com/osiris/desku/VerticalLayout.css"
             //styles = Stream.toString(App.getResource(VerticalLayout.class.toString().replace(".","/") + ".css"));
             App.appendToGlobalStyles(styles);
